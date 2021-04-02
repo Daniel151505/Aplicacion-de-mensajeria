1726030860d589cc6f443a612aeb98d3a6129d28
Daniel151505 <daniel150520011@gmail.com>
2021-04-01 23:41:50 -0500

    Creacion del boton login con Twitter

--- aplicacionMensajeria/src/pages/Login.vue
+++ aplicacionMensajeria/src/pages/Login.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+
    <div class="jumbotron bg-primary text-while">
      <h2 class="lead display-3">Aplicación de Mensajeria</h2>
      <p>Comunicación en tiempo real</p>
@@ -12,7 +13,11 @@
       </div>
     </div>
 
-
+  <div class="row">
+      <div class="div text-center">
+        <button class="btn btn-outline-danger btn-lg">Login con Twitter</button>
+      </div>
+    </div>
   </div>
 
 </div>
