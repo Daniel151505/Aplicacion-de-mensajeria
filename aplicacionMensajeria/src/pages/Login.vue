e9ff4abaea4025c502cfe3a08c992d9079223663
Daniel151505 <daniel150520011@gmail.com>
2021-04-01 23:41:00 -0500

    Creación del boton Login con Google

--- aplicacionMensajeria/src/pages/Login.vue
+++ aplicacionMensajeria/src/pages/Login.vue
@@ -4,6 +4,17 @@
      <h2 class="lead display-3">Aplicación de Mensajeria</h2>
      <p>Comunicación en tiempo real</p>
   </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="div text-center">
+        <button class="btn btn-outline-danger btn-lg">Login con Google</button>
+      </div>
+    </div>
+
+
+  </div>
+
 </div>
 </template>
 
