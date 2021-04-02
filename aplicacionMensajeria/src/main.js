8cdd6d4fb0cbc123c95a22cce15dce557692af6c
Daniel151505 <daniel150520011@gmail.com>
2021-04-02 15:04:31 -0500

    Configuracion firebase

--- aplicacionMensajeria/src/main.js
+++ aplicacionMensajeria/src/main.js
@@ -1,11 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import firebase from 'firebase/app'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import firebase from 'firebase/app';
+
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBaaKDI5VcW1-k7gI4wrU1bnobCCl0YK-E",
+  authDomain: "aplicacion-de-mensajeria-9d804.firebaseapp.com",
+  projectId: "aplicacion-de-mensajeria-9d804",
+  storageBucket: "aplicacion-de-mensajeria-9d804.appspot.com",
+  messagingSenderId: "82177931717",
+  appId: "1:82177931717:web:a445164da08aab79a773ac",
+  measurementId: "G-XR5B2T56ST"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 /* eslint-disable no-new */
 new Vue({
@@ -14,3 +28,6 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+
+
