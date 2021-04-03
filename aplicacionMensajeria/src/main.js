d8c8438642fdb9993e8d4820c5948c56f5326021
Daniel151505 <daniel150520011@gmail.com>
2021-04-02 21:18:54 -0500

    Fix error

--- aplicacionMensajeria/src/main.js
+++ aplicacionMensajeria/src/main.js
@@ -1,10 +1,7 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 import firebase from 'firebase/app';
-
 
 Vue.config.productionTip = false
 
@@ -19,6 +16,7 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+window.firebase = firebase;
 
 
 /* eslint-disable no-new */
